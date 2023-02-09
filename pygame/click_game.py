b3a7972d8f3df55afc4eb9c68c20d9ea4471e20c
khadija-D <113620416+khadija-D@users.noreply.github.com>
2023-02-09 09:17:06 -0800

    Add files via upload

--- pygame/click_game.py
+++ pygame/click_game.py
@@ -5,7 +5,7 @@ WIDTH = 500
 HEIGHT = Tina_the_Blob.height + 20
 
 def draw():
-    screen.clear()
+    screen.clear
     screen.fill((0, 0, 255))
     Tina_the_Blob.draw()
 
@@ -13,5 +13,17 @@ def update():
     Tina_the_Blob.left = Tina_the_Blob.left + 2
     if Tina_the_Blob.left > WIDTH:
         Tina_the_Blob.right = 0
-
+def on_mouse_down(pos):
+    if Tina_the_Blob.collidepoint(pos):
+        print ("Eek!")
+        sounds.eep.play()
+        set_character_clicked()
+    else:
+        print("LMAO")
+def set_character_clicked():
+    Tina_the_Blob.image = 'character_clicked'
+    sounds.eep.play()
+    clock.schedule_unique(set_character,1)
+def set_character():
+    Tina_the_Blob.image = 'character'
 
